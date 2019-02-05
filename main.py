70f1c971423773398a70730ed51b54960eaa81c3
Garon Power <garonpower@yahoo.com>
2019-02-05 23:19:52 +0000

    created main.py and templates

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:h7mfGQuWUn5UEsee@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    content = db.Column(db.String(1000))
+    posted = db.Column(db.Boolean)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+        self.posted = False
+
+@app.route('/main')
+def main_blog_page():   
+    return redirect('/')
+
+@app.route('/newpost')
+def add_blog_entry():   
+    return redirect('/')
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = Blog(post_title,post_content)
+        db.session.add(new_post)
+        db.session.commit()
+
+    posts = Blog.query.filter_by(posted=False).all()
+    posted_blogs = Blog.query.filter_by(posted=True).all()
+    return render_template('newpost.html',title="Build A Blog", 
+        posts=posts, posted_blogs=posted_blogs)
+
+@app.route('/completed_posts', methods=['POST'])
+def completed_posts():
+
+    title_id = int(request.form['title-id'])
+    title_post = Blog.query.get(title_id)
+    content_id = int(request.form['content-id'])
+    content_post = Blog.query.get(content_id)
+    title_post.completed = True
+    content_post.completed = True
+    db.session.add(title_post)
+    db.session.add(content_post)
+    db.session.commit()
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
